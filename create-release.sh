24f8dacf493e73b793678bea9fb2cbfef2ff2ab9
Marcelo Matos <marcelofmatos@gmail.com>
2025-07-25 09:00:39 -0400

    fix(create-release.sh): corrigir fluxo de execução ao verificar tipo de versão

--- create-release.sh
+++ create-release.sh
@@ -4,8 +4,9 @@
 if [ -z "$1" ]; then
     echo "Uso: $0 [version_type]"
     echo "version_type: patch , minor, major"
-else
-    version_type="$1"
+    exit 1
 fi
 
+version_type="$1"
 gh workflow run "Create Release" -f version_type="$version_type"
+
